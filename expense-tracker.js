fbfc8192e4aee179253dbf01b225435a678c9b39
Ibrahim Bocoum <ibrahimbocoum10@gmail.com>
2020-01-07 23:22:20 -0500

    update the expense-track file

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,25 +1,40 @@
 const account ={
     name:'Ibrahim Bocoum' ,
     expenses:[],
+    income:[],
+    addIncome: function(description, amount){
+        this.income.push({
+            description: description,
+            amount: amount,
+
+        })
+
+    },
     addExpense: function (description, amount){
         this.expenses.push({
             description: description,
             amount :amount
+})
 
-        })
 
  },
  getAccountSummary:function(){
-     let totalExpenses = 0
+     let firstExpenses = 0;
+     let secondIcome = 0;
+     let finalExpense = 0;
 
-     this.expenses.forEach(function(expenses){
-         totalExpenses = totalExpenses + expenses.amount
-
+this.expenses.forEach(function(expenses){
+         firstExpenses = firstExpenses + expenses.amount
+})
+     this.expenses.forEach(function(income){
+         secondIcome = secondIcome + income.amount
      })
- 
-     return `${this.name} has $${totalExpenses} in expenses`
+     finalExpense = firstExpenses - secondIcome 
+     return `${this.name} has $${finalExpense} in expenses`
 
  }
+
+
 }
 // expense => description , amount
 // addExpense => description amount
@@ -27,4 +42,7 @@ const account ={
 
 account.addExpense('Rent',950);
 account.addExpense('coffe',2);
+account.addExpense('medicine',30);
+account.addIncome('job', 300);
+
 console.log(account.getAccountSummary());
