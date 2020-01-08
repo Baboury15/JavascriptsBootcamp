bdce6116c8af67c0d4c2b501e34a1005af1bec2f
Ibrahim Bocoum <ibrahimbocoum10@gmail.com>
2020-01-08 00:16:03 -0500

    new update on index.htlm and note app.js

--- note-app.js
+++ note-app.js
@@ -1,1 +1,15 @@
 console.log("this is javascript document")
+
+//query and remove
+const p = document.querySelector('p')
+//p.remove();
+console.log(p)
+
+const paragraph = document.querySelectorAll('p');
+paragraph.forEach((p)=>{
+    p.textContent = "********"
+})
+
+const newPara = document.createElement('p')
+newPara.textContent = 'this is a new paragraph in javascript '
+document.querySelector('body').appendChild(newPara)
