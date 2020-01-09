55200a7e81e909fb1314bfecdf445daff8a47f07
Ibrahim Bocoum <ibrahimbocoum10@gmail.com>
2020-01-09 00:01:33 -0500

    create two version of person

--- oop/person.js
+++ oop/person.js
@@ -1,6 +1,9 @@
-const person = function(){
+const person = function(firstname){
+    this.firstName = firstname
 
 }
 
-const me = new  person ()
-console.log(me)
+const me = new person ('Ibrahim')
+me1 = new person('malick')
+console.log(me)  
+console.log(me1)  
